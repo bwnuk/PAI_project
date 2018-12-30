57ee51710fb4563a9bd6ac356ba803c80c6b28e9
Bartłomiej Wnuk <bartekwnuk13@gmail.com>
2018-12-30 14:56:38 +0100

    Project init

---
+++ backend/src/main/java/pl/krakow/politechnika/wnuk/pai/booker/BookerApplication.java
@@ -0,0 +1,14 @@
+package pl.krakow.politechnika.wnuk.pai.booker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookerApplication.class, args);
+    }
+
+}
+
