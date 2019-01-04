d1a48e56f29316a1d779b2fd4c3eb9c1d148bc41
bwnuk <bartekwnuk13@gmail.com>
2019-01-04 22:44:58 +0100

    Front init

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap-grid.min.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1 className="alert alert-warning">Welcome</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
