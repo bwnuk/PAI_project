f830fe338bdf159a6f8fa755b8c379f1491e35d9
bwnuk <bartekwnuk13@gmail.com>
2019-01-10 19:22:17 +0100

    Navbar start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap-grid.min.css"
+import Navbar from "./components/Navbar";
+import CategoryComponent from "./components/CategoryComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1 className="alert alert-warning">Welcome</h1>
+        <Navbar/>
+        <CategoryComponent/>
       </div>
     );
   }
---
+++ frontend/src/components/CategoryComponent.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import '../index.css'
+
+class CategoryComponent extends Component{
+    render(){
+        return(
+            <div className=".center">
+            </div>
+        );
+    }
+}
+
+export default CategoryComponent;
