fb71a45446a6883e85d44182ba893fc47be6adcc
bwnuk <bartekwnuk13@gmail.com>
2019-01-11 16:53:31 +0100

    Categories are read from DB

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap-grid.min.css"
 import Navbar from "./components/Navbar";
-import CategoryComponent from "./components/CategoryComponent";
+import CategoryComponent from "./components/category/CategoryComponent";
 import TopComponent from "./components/TopComponent";
 import LoginComponent from "./components/login/LoginComponent";
 import SearchComponent from "./components/SearchComponent";
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-        <Router>
-            <div className="App">
-                <Navbar/>
-                <Route exact path="/top" component={TopComponent}/>
-                <Route exact path="/login" component={LoginComponent}/>
-                <Route exact path="/category" component={CategoryComponent}/>
-                <Route exact path="/search" component={SearchComponent}/>
-            </div>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <div className="App">
+                    <Navbar/>
+                    <Route exact path="/top" component={TopComponent}/>
+                    <Route exact path="/login" component={LoginComponent}/>
+                    <Route exact path="/category" component={CategoryComponent}/>
+                    <Route exact path="/search" component={SearchComponent}/>
+                </div>
+            </Router>
+        </Provider>
     );
   }
 }
---
+++ frontend/src/components/book/BookComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import '../../index.css'
+
+class BookComponent extends Component{
+    render(){
+        return(
+            <div className="center">
+            </div>
+        );
+    }
+}
