508d200897f1d04185f1ee3dc02e665fd54c97ba
bwnuk <bartekwnuk13@gmail.com>
2019-01-14 22:19:06 +0100

    Stars corrected.

--- frontend/src/components/book/BookItem.js
+++ frontend/src/components/book/BookItem.js
@@ -3,14 +3,18 @@ import Rater from 'react-rater'
 import 'react-rater/lib/react-rater.css'
 
 export const BookItem = ({booktitle, authorfname, authorsname, publisher}) =>{
+
     return (
         <div className="card">
             <div className="card-body">
-                <h4 className="card-title">{booktitle}</h4>
+                <img className="card-img-top" src="..\..\..\public\jobs.jpg" width="100" height="100" alt="Card image cap"/>
+                <h5 className="card-title">{booktitle}</h5>
                 <p className="card-text"><b>Autor:</b> {authorfname} {authorsname} </p>
                 <p className="card-text"><b>Wydawnictwo:</b> {publisher}</p>
                 <p className="card-text"><b>Ocena:</b>4.5</p>
-                <Rater total={5} rating={1}/>
+                <Rater total={5} rating={1} onRate={({rating})=>{
+                    console.log(rating+ " " + booktitle);
+                }}/>
             </div>
         </div>
     );
